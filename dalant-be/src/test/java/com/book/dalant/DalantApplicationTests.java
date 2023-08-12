e535d3199d6858168a794d565dd3d58fda108da6
hyojin52 <khj960502@naver.com>
2023-08-12 18:42:25 +0900

    feat : [Dev-1] 환경(local, prod) 별 yml 파일 추가

---
+++ dalant-be/src/test/java/com/book/dalant/DalantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.dalant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
